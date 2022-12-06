b3147cd6ae04ddc9e2e3e67f3719e98e8f14a8c3
Gabriel <Gabriel.Senekovic@gmail.com>
2022-12-06 22:40:32 +0100

    Lanterns can now be used as switches

--- Assets/Scripts/Objects/Fireplace.cs
+++ Assets/Scripts/Objects/Fireplace.cs
@@ -1,47 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class PuzzleSwitch : MonoBehaviour, IAttackable, ISwitch
+public class Fireplace : MonoBehaviour, IFlammable, ISwitch
 {
     [SerializeField] List<Activatable> activatables = new List<Activatable>();
-    public bool isHit;
+    [SerializeField] bool onFire;
     Animator anim;
-
-    // Start is called before the first frame update
+    Fire fire;
     void Start()
     {
-        anim = GetComponent<Animator>();
-        isHit = false;
+        fire = GetComponentInChildren<Fire>();
+        anim = fire.gameObject.GetComponent<Animator>();
+        fire.gameObject.SetActive(onFire);
     }
-
     public void ActivateSwitch()
     {
-        isHit = true;
-        AudioManager.PlaySFX("SwitchHit");
         Game.Instance.cinemachineVirtualCamera.gameObject.GetComponent<CinemachineConfiner>().m_ConfineScreenEdges = false;
         StartCoroutine("ActivationCutscene");
         anim.SetTrigger("Activate");
     }
 
-    public void OnBeAttacked(int value, Vector2 dir)
+    public void SetOnFire()
     {
-        if(!isHit)
-        {
-            ActivateSwitch(); //This dowes noting atm the switch is activated in playerattack
-        }
+        onFire = true;
+        fire.gameObject.SetActive(onFire);
+        ActivateSwitch();
     }
     public IEnumerator ActivationCutscene() //The same as activate when defeat enemies
     {
         Transform temp = Game.Instance.cinemachineVirtualCamera.Follow;
         yield return new WaitForSeconds(1.0f);
-        for(int i = 0; i < activatables.Count; i++)
+        for (int i = 0; i < activatables.Count; i++)
         {
             Game.Instance.cinemachineVirtualCamera.Follow = activatables[i].transform;
             yield return new WaitForSeconds(0.4f);
             activatables[i].Activate();
-            if(i == activatables.Count - 1)
+            if (i == activatables.Count - 1)
             {
                 AudioManager.PlaySFX("Discovery");
             }
